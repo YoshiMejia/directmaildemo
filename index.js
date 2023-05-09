c6fafa5850f990b38b0bec06d790040a425c02b5
Yoshi <yoshimejia00@gmail.com>
2023-05-09 15:46:37 -0700

    first commit: separated into modules

---
+++ index.js
@@ -0,0 +1,122 @@
+const Converter = require('csv-converter-to-pdf-and-html');
+const path = require('path');
+const express = require('express');
+const port = 8000;
+const fs = require('fs');
+const csv = require('csv-parser');
+const handlebars = require('handlebars');
+const { engine } = require('express-handlebars');
+const multer = require('multer');
+const { log } = require('console');
+
+const templateCompiler = require('./helpers/templateCompiler');
+
+const app = express();
+app.engine(
+  'handlebars',
+  engine({ extname: '.hbs', defaultLayout: 'template' })
+);
+app.set('view engine', 'handlebars');
+
+app.use(express.static('public/uploads')); // makes this directory the static directory for uploads
+app.use('/converted', express.static('converted')); // Serve static files in the 'converted' directory
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/uploads');
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+var upload = multer({ storage: storage }).single('csv');
+
+app.get('/', (req, res) => {
+  res.sendFile('index.html', { root: __dirname });
+  //the .sendFile method needs the absolute path to the file
+});
+
+const convertedRouter = express.Router(); // Define a new router for showing the converted files
+convertedRouter.get('/success', (req, res) => {
+  // Read the contents of the 'converted' directory
+  fs.readdir('converted', (err, files) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send('Error retrieving files');
+    } else {
+      // Generate an HTML response with a list of the files
+      const fileList = files
+        .map(
+          (filename) =>
+            `<li><a href="/converted/${filename}">${filename}</a></li>`
+        )
+        .join('');
+      const html = `
+              <html>
+                <head>
+                  <title>Converted Files</title>
+                </head>
+                <body>
+                  <h1>Converted Files</h1>
+                  <ul>
+                    ${fileList}
+                  </ul>
+                </body>
+              </html>
+            `;
+      res.send(html);
+    }
+  });
+});
+
+// Use the new router for the '/converted' route
+app.use('/converted', convertedRouter);
+
+app.post('/convert', (req, res) => {
+  let count = 0;
+  upload(req, res, (err) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send('Error processing file upload');
+    } else {
+      const data = [];
+      // Grab name of template that was selected by user
+      const selectedTemplate = req.body.template;
+      // Use the appropriate template function to generate the HTML content
+      const source = fs.readFileSync(
+        path.join(__dirname, 'views/layouts', `${selectedTemplate}.hbs`),
+        'utf8'
+      );
+      const template = handlebars.compile(source);
+      // Parse the CSV file and create an HTML file for each row
+      fs.createReadStream(req.file.path)
+        .pipe(csv())
+        .on('data', (row) => {
+          count++;
+          console.log(
+            'inside .post/convert, looking at the row variable:',
+            row
+          );
+          const html = templateCompiler(selectedTemplate, row, template);
+          const outputName = `output-rowNum-${count}.html`;
+          const outputPath = path.join(__dirname, 'converted', outputName);
+          fs.writeFile(outputPath, html, (err) => {
+            if (err) {
+              console.log(err);
+            } else {
+              console.log(`File ${outputName} created successfully`);
+            }
+          });
+          data.push(row);
+        })
+        .on('end', () => {
+          console.log('CSV file successfully processed');
+        });
+      res.redirect('/converted/success');
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Now listening on port ${port}`);
+});
