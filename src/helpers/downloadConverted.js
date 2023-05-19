e4e24dc3233e3bcdf05f1aac3f5a09ff0305f51c
Yoshi <yoshimejia00@gmail.com>
2023-05-18 20:57:52 -0700

    adding in functionality to send ZIP file over to client

--- src/helpers/downloadConverted.js
+++ src/helpers/downloadConverted.js
@@ -1,45 +1,60 @@
 const archiver = require('archiver');
 const path = require('path');
 const fs = require('fs');
+const fsX = require('fs-extra');
 
 const downloadConverted = (req, res) => {
-  const folderName = 'converted';
-  const folderPath = path.join(path.resolve(__dirname, '..'), 'converted');
+  const folderName = req.query.folderName || 'converted';
+  const folderPath = path.join(path.resolve(__dirname, '..'), folderName);
   const zipName = folderName + '.zip';
-  const zipPath = path.resolve(__dirname, '..', 'converted', zipName);
-  // Create a new Archiver instance with the 'zip' format and compression level of 9
+  const zipPath = path.resolve(__dirname, '..', zipName);
   const archive = archiver('zip', { zlib: { level: 9 } });
-  // Create a writable stream to save the ZIP file to disk
   const output = fs.createWriteStream(zipPath);
-  // Pipe the archive to the output stream
+
   archive.pipe(output);
-  // Read all files in the folder and add them to the archive
+
   fs.readdir(folderPath, (err, files) => {
     if (err) {
       console.error(err);
-      res.status(500).send('Error creating ZIP file');
-      return;
+      return res.status(500).send('Error reading directory');
     }
-    // Add each file to the archive with the same name as the original file
+
     files.forEach((filename) => {
       const filePath = path.join(folderPath, filename);
       archive.file(filePath, { name: filename });
     });
-    // Finalize the archive once all files have been added
+
     archive.finalize();
   });
-  // Send the ZIP file to the client for download once the output stream is closed
+
   output.on('close', () => {
-    res.download(zipPath);
+    console.log('ZIP file created successfully');
+    res.download(zipPath, (err) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Error sending file');
+      } else {
+        console.log('ZIP file sent successfully');
+        fs.unlink(zipPath, (err) => {
+          if (err) {
+            console.error('Error deleting ZIP file:', err);
+          } else {
+            console.log('ZIP file deleted successfully');
+            fsX
+              .emptyDir(folderPath)
+              .then(() => console.log('Converted Folder cleared successfully'))
+              .catch((err) =>
+                console.error('Error clearing Converted Folder:', err)
+              );
+          }
+        });
+      }
+    });
   });
 
   archive.on('error', (err) => {
     console.error(err);
     res.status(500).send('Error creating ZIP file');
-  });
-
-  archive.on('finish', () => {
-    console.log(`ZIP file ${zipPath} created`);
   });
 };
 
